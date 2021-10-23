d08e6aadcb58e1edb9cd177ecef40cd92eaa0f36
Rikardian <92721408+Rikardian@users.noreply.github.com>
2021-10-23 17:00:52 +0400

    ДЗ Блок 2. Добавил анонимный класс в Solution

--- src/HomeWork1/Solution.java
+++ src/HomeWork1/Solution.java
@@ -18,5 +18,49 @@ public class Solution {
             employeesList.add(employee);
         }
 
+        Employee anonymousEmployee = new Employee() {
+            String name = "Савелий";
+            int salary = 60000;
+            boolean sex = true;
+
+
+            @Override
+            public void work() {
+                System.out.println("Анонимный класс делает работу");
+            }
+
+            public boolean isWorking (){
+                return true;
+            }
+
+            public void getSalary(){
+                if (getSalaryDate().equals("23")){
+                    System.out.println("Анонимный класс получил зарплату");
+                }
+                else System.out.println("День зарплаты будет позднее");
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public void setSalary(int salary) {
+                this.salary = salary;
+            }
+
+            public boolean isSex() {
+                return sex;
+            }
+
+            public void setSex(boolean sex) {
+                this.sex = sex;
+            }
+        };
+
     }
 }
